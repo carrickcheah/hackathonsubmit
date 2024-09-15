e3b0dc5387e86ba314e930a15b2f3c172baa5f5c
Carrick Cheah <138642124+carrickcheah@users.noreply.github.com>
2024-09-16 02:56:23 +0800

    Add files via upload

---
+++ backend/static/assets/NoPage-5e1fa5a0.js
@@ -0,0 +1,2 @@
+import{bH as o}from"./vendor-173803c2.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-5e1fa5a0.js.map
